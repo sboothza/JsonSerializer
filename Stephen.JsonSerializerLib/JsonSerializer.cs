60c4e0c967695c51bfc07d572335098fc9a38cc1
stephen <stephen.booth.za@gmail.com>
2022-09-06 15:12:33 +0200

    formatting

--- Stephen.JsonSerializerLib/JsonSerializer.cs
+++ Stephen.JsonSerializerLib/JsonSerializer.cs
@@ -7,92 +7,89 @@ using System.Text;
 
 namespace Stephen.JsonSerializer
 {
-    public class JsonSerializer
-    {
-        public List<Type> IgnoreTypes { get; }
+	public class JsonSerializer
+	{
+		public List<Type> IgnoreTypes { get; }
 
-        public JsonSerializer(params Type[] ignoreTypes) { IgnoreTypes = new List<Type>(ignoreTypes); }
+		public JsonSerializer(params Type[] ignoreTypes)
+		{
+			IgnoreTypes = new List<Type>(ignoreTypes);
+		}
 
-        public string Serialize(object source)
-        {
-            var writer = new StringWriter(new StringBuilder(1024));
-            Serialize(source, writer);
-            return writer.ToString();
-        }
+		public string Serialize(object source)
+		{
+			var writer = new StringWriter(new StringBuilder(1024));
+			Serialize(source, writer);
+			return writer.ToString();
+		}
 
-        public void Serialize(object source, TextWriter writer)
-        {
-            if (source is null)
-            {
-                writer.Write("null");
-                return;
-            }
+		public void Serialize(object source, TextWriter writer)
+		{
+			if (source is null)
+			{
+				writer.Write("null");
+				return;
+			}
 
-            if (IgnoreTypes.Any(t => t.IsInstanceOfType(source)))
-                return;
+			if (IgnoreTypes.Any(t => t.IsInstanceOfType(source)))
+				return;
 
-            if (source.Flatten(out var result))
-            {
-                writer.Write(result);
-                return;
-            }
+			if (source.Flatten(out var result))
+			{
+				writer.Write(result);
+				return;
+			}
 
-            //complex
-            if (source is IEnumerable sourceEnumerable)
-            {
-                if (sourceEnumerable is IDictionary dictionarySource)
-                {
-                    //handle dictionary
-                    writer.Write("{");
+			//complex
+			if (source is IEnumerable sourceEnumerable)
+			{
+				if (sourceEnumerable is IDictionary dictionarySource)
+				{
+					//handle dictionary
+					writer.Write("{");
 
-                    dictionarySource.GetEnumerator()
-                                    .Cast<DictionaryEntry>()
-                                    .Where(p => !IgnoreTypes.Any(t => t.IsInstanceOfType(p.Value)))
-                                    .Delimit((entry, wr) =>
-                                             {
-                                                 wr.Write($"\"{entry.Key}\":");
-                                                 Serialize(entry.Value, wr);
-                                             },
-                                             writer,
-                                             ",");
+					dictionarySource.GetEnumerator()
+									.Cast<DictionaryEntry>()
+									.Where(p => !IgnoreTypes.Any(t => t.IsInstanceOfType(p.Value)))
+									.Delimit((entry, wr) =>
+											 {
+												 wr.Write($"\"{entry.Key}\":");
+												 Serialize(entry.Value, wr);
+											 }, writer, ",");
 
-                    writer.Write("}");
-                    return;
-                }
+					writer.Write("}");
+					return;
+				}
 
-                //handle list
-                writer.Write("[");
+				//handle list
+				writer.Write("[");
 
-                sourceEnumerable.Cast<object>()
-                                .Where(i => !IgnoreTypes.Any(t => t.IsInstanceOfType(i)))
-                                .Delimit(Serialize,
-                                         writer,
-                                         ",");
+				sourceEnumerable.Cast<object>()
+								.Where(i => !IgnoreTypes.Any(t => t.IsInstanceOfType(i)))
+								.Delimit(Serialize, writer, ",");
 
-                writer.Write("]");
-                return;
-            }
+				writer.Write("]");
+				return;
+			}
 
-            //single object
-            writer.Write("{");
+			//single object
+			writer.Write("{");
 
-            source.GetType()
-                  .GetProperties()
-                  .Select(prop => new
-                                  {
-                                      Name = prop.Name,
-                                      Value = source.GetFieldOrPropertyValue(prop.Name)
-                                  })
-                  .Where(p => !IgnoreTypes.Any(t => t.IsInstanceOfType(p.Value)))
-                  .Delimit((item, wr) =>
-                           {
-                               wr.Write($"\"{item.Name}\":");
-                               Serialize(item.Value, wr);
-                           },
-                           writer,
-                           ",");
+			source.GetType()
+				  .GetProperties()
+				  .Select(prop => new
+				  {
+					  Name = prop.Name,
+					  Value = source.GetFieldOrPropertyValue(prop.Name)
+				  })
+				  .Where(p => !IgnoreTypes.Any(t => t.IsInstanceOfType(p.Value)))
+				  .Delimit((item, wr) =>
+						   {
+							   wr.Write($"\"{item.Name}\":");
+							   Serialize(item.Value, wr);
+						   }, writer, ",");
 
-            writer.Write("}");
-        }
-    }
+			writer.Write("}");
+		}
+	}
 }
--- Stephen.JsonSerializerLib/TypeExtensions.cs
+++ Stephen.JsonSerializerLib/TypeExtensions.cs
@@ -1,105 +1,116 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Stephen.JsonSerializer
 {
-    public static class TypeExtensions
-    {
-        public static bool Flatten(this object source, out string value)
-        {
-            switch (source.GetType())
-            {
-                case { IsPrimitive: true }:
-                case { IsEnum: true }:
-                case {} decimalType when decimalType == typeof(decimal):
-                    value = $"{source}";
-                    return true;
+	public static class TypeExtensions
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool Flatten(this object source, out string value)
+		{
+			switch (source.GetType())
+			{
+				case { IsPrimitive: true }:
+				case { IsEnum: true }:
+				case { } decimalType when decimalType == typeof(decimal):
+					value = $"{source}";
+					return true;
 
-                case {} stringType when stringType == typeof(string):
-                    value = $"\"{source}\"";
-                    return true;
+				case { } stringType when stringType == typeof(string):
+					value = $"\"{source}\"";
+					return true;
 
-                case {} dtType when dtType == typeof(DateTime):
-                    value = $"\"{(DateTime)source:O}\"";
-                    return true;
+				case { } dtType when dtType == typeof(DateTime):
+					value = $"\"{(DateTime)source:O}\"";
+					return true;
 
-                default:
-                    value = null;
-                    return false;
-            }
-        }
+				default:
+					value = null;
+					return false;
+			}
+		}
 
-        public static bool IsSimple(this Type type)
-        {
-            return type.IsPrimitive        ||
-                   type.IsEnum             ||
-                   type == typeof(string)  ||
-                   type == typeof(decimal) ||
-                   type.IsValueType;
-        }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static bool IsSimple(this Type type)
+		{
+			return type.IsPrimitive ||
+				   type.IsEnum ||
+				   type == typeof(string) ||
+				   type == typeof(decimal) ||
+				   type.IsValueType;
+		}
 
-        public static MemberInfo GetFieldOrProperty(this Type type, string name)
-        {
-            var prop = type.GetProperty(name);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static MemberInfo GetFieldOrPropertyInfo(this Type type, string name)
+		{
+			var prop = type.GetProperty(name);
 
-            if (prop != null)
-                return prop;
+			if (prop != null)
+				return prop;
 
-            return type.GetField(name);
-        }
+			var field = type.GetField(name);
+			if (field != null)
+				return field;
 
-        public static object GetFieldOrPropertyValue(this object source, string name)
-        {
-            try
-            {
-                var type = source.GetType();
-                var prop = type.GetProperty(name);
+			return null;
+		}
 
-                if (prop != null)
-                    return prop.GetValue(source, null);
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static object GetFieldOrPropertyValue(this object source, string name)
+		{
+			try
+			{
+				var prop = source.GetType()
+								 .GetProperty(name);
 
-                var field = type.GetField(name);
+				if (prop != null)
+					return prop.GetValue(source, null);
 
-                return field?.GetValue(source);
-            }
-            catch
-            {
-                return null;
-            }
-        }
+				var field = source.GetType()
+								  .GetField(name);
 
-        public static void SetFieldOrProperty<T>(this Type type, string name, T value)
-        {
-            var targetProperty = type.GetProperty(name);
-            if (targetProperty != null)
-                targetProperty.SetValue(type, value, null);
+				return field?.GetValue(source);
+			}
+			catch
+			{
+				return null;
+			}
+		}
 
-            var targetField = type.GetField(name);
-            if (targetField != null)
-                targetField.SetValue(type, value);
-        }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void SetFieldOrProperty<T>(this Type type, string name, T value)
+		{
+			var targetProperty = type.GetProperty(name);
+			if (targetProperty != null)
+				targetProperty.SetValue(type, value, null);
 
-        public static void DumpProperties(this Type type)
-        {
-            var props = type.GetProperties()
-                            .Select(p => new
-                                         {
-                                             Name = p.Name,
-                                             TypeName = p.PropertyType.Name
-                                         })
-                            .Union(type.GetFields()
-                                       .Select(p => new
-                                                    {
-                                                        Name = p.Name,
-                                                        TypeName = p.FieldType.Name
-                                                    }))
-                            .ToList();
+			var targetField = type.GetField(name);
+			if (targetField != null)
+				targetField.SetValue(type, value);
+		}
 
-            foreach (var prop in props)
-            {
-                Console.WriteLine($"public {prop.TypeName} {prop.Name} {{get; set;}}");
-            }
-        }
-    }
+		public static void DumpProperties(this Type type)
+		{
+			var props = type.GetProperties()
+							.Select(p => new
+							{
+								Name = p.Name,
+								TypeName = p.PropertyType.Name
+							})
+							.Union(type.GetFields()
+									   .Select(p => new
+									   {
+										   Name = p.Name,
+										   TypeName = p.FieldType.Name
+									   }))
+							.ToList();
+
+			foreach (var prop in props)
+			{
+				Console.WriteLine($"public {prop.TypeName} {prop.Name} {{get; set;}}");
+			}
+		}
+	}
 }
