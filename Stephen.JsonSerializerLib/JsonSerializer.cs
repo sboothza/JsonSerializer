e05ef1c1b303ba9465ea46c2cba136ed1ac68458
sgb-spin <144226803+sgb-spin@users.noreply.github.com>
2023-11-10 14:13:59 +0200

    SB: Cleaned up code and added ignoreerrors

--- Stephen.JsonSerializerLib/JsonSerializer.cs
+++ Stephen.JsonSerializerLib/JsonSerializer.cs
@@ -1,95 +1,85 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace Stephen.JsonSerializer
 {
-	public class JsonSerializer
-	{
-		public List<Type> IgnoreTypes { get; }
+    public static class JsonSerializer
+    {
+        public static string Serialize(object source, JsonSerializerOptions options = null)
+        {
+            var writer = new StringWriter(new StringBuilder(1024));
+            Serialize(source, writer, options);
+            return writer.ToString();
+        }
 
-		public JsonSerializer(params Type[] ignoreTypes)
-		{
-			IgnoreTypes = new List<Type>(ignoreTypes);
-		}
+        private static void Serialize(object source, TextWriter writer, JsonSerializerOptions options)
+        {
+            if (source is null)
+            {
+                writer.Write("null");
+                return;
+            }
 
-		public string Serialize(object source)
-		{
-			var writer = new StringWriter(new StringBuilder(1024));
-			Serialize(source, writer);
-			return writer.ToString();
-		}
+            options ??= new JsonSerializerOptions();
 
-		public void Serialize(object source, TextWriter writer)
-		{
-			if (source is null)
-			{
-				writer.Write("null");
-				return;
-			}
+            if (options.IgnoreTypes.Any(t => t.IsInstanceOfType(source)))
+                return;
 
-			if (IgnoreTypes.Any(t => t.IsInstanceOfType(source)))
-				return;
+            if (source.Flatten(out var result))
+            {
+                writer.Write(result);
+                return;
+            }
 
-			if (source.Flatten(out var result))
-			{
-				writer.Write(result);
-				return;
-			}
+            //complex
+            if (source is IEnumerable sourceEnumerable)
+            {
+                if (sourceEnumerable is IDictionary dictionarySource)
+                {
+                    //handle dictionary
+                    writer.Write("{");
 
-			//complex
-			if (source is IEnumerable sourceEnumerable)
-			{
-				if (sourceEnumerable is IDictionary dictionarySource)
-				{
-					//handle dictionary
-					writer.Write("{");
+                    dictionarySource.GetEnumerator()
+                                    .Cast<DictionaryEntry>()
+                                    .Where(p => !options.IgnoreTypes.Any(t => t.IsInstanceOfType(p.Value)))
+                                    .DelimitToWriter((entry, wr) =>
+                                    {
+                                        wr.Write($"\"{entry.Key}\":");
+                                        Serialize(entry.Value, wr, options);
+                                    }, writer, ",");
 
-					dictionarySource.GetEnumerator()
-									.Cast<DictionaryEntry>()
-									.Where(p => !IgnoreTypes.Any(t => t.IsInstanceOfType(p.Value)))
-									.Delimit((entry, wr) =>
-											 {
-												 wr.Write($"\"{entry.Key}\":");
-												 Serialize(entry.Value, wr);
-											 }, writer, ",");
+                    writer.Write("}");
+                    return;
+                }
 
-					writer.Write("}");
-					return;
-				}
+                //handle list
+                writer.Write("[");
 
-				//handle list
-				writer.Write("[");
+                sourceEnumerable.Cast<object>()
+                                .Where(i => !options.IgnoreTypes.Any(t => t.IsInstanceOfType(i)))
+                                .DelimitToWriter((entry, wr) => Serialize(entry, wr, options), writer, ",");
 
-				sourceEnumerable.Cast<object>()
-								.Where(i => !IgnoreTypes.Any(t => t.IsInstanceOfType(i)))
-								.Delimit(Serialize, writer, ",");
+                writer.Write("]");
+                return;
+            }
 
-				writer.Write("]");
-				return;
-			}
+            //single object
+            writer.Write("{");
 
-			//single object
-			writer.Write("{");
+            source.GetType()
+                  .GetProperties()
+                  .Select(prop => PropertyTuple.Create(options, source, prop.Name))
+                  .Where(p => !options.IgnoreTypes.Any(t => t.IsInstanceOfType(p.Value)))
+                  .Where(p => p != null)
+                  .DelimitToWriter((item, wr) =>
+                  {
+                      wr.Write($"\"{item.Name}\":");
+                      Serialize(item.Value, writer, options);
+                  }, writer, ",");
 
-			source.GetType()
-				  .GetProperties()
-				  .Select(prop => new
-				  {
-					  Name = prop.Name,
-					  Value = source.GetFieldOrPropertyValue(prop.Name)
-				  })
-				  .Where(p => !IgnoreTypes.Any(t => t.IsInstanceOfType(p.Value)))
-				  .Delimit((item, wr) =>
-						   {
-							   wr.Write($"\"{item.Name}\":");
-							   Serialize(item.Value, wr);
-						   }, writer, ",");
-
-			writer.Write("}");
-		}
-	}
+            writer.Write("}");
+        }
+    }
 }
---
+++ Stephen.JsonSerializerLib/PropertyTuple.cs
@@ -0,0 +1,28 @@
+using System;
+namespace Stephen.JsonSerializer;
+
+class PropertyTuple
+{
+    public string Name { get; }
+    public object Value { get; }
+
+    private PropertyTuple(object source, string name)
+    {
+        Name = name;
+        Value = source.GetFieldOrPropertyValue(Name);
+    }
+
+    public static PropertyTuple Create(JsonSerializerOptions options, object source, string name)
+    {
+        try
+        {
+            return new PropertyTuple(source, name);
+        }
+        catch (Exception)
+        {
+            if (options.IgnoreErrors)
+                return null;
+            throw;
+        }
+    }
+}
